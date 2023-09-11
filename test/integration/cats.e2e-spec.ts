48c02dd06127f4543378cbfdff4d8f42fe555d2c
Frank Garcia <garcia1901l@gmail.com>
2023-09-10 22:20:43 -0500

    test unit service users

--- test/integration/cats.e2e-spec.ts
+++ test/integration/cats.e2e-spec.ts
@@ -1,17 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '../../src/app.module'; // Ajusta la ruta a tu AppModule
-import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../../src/app.module';
 import * as request from 'supertest';
+//import { AuthController } from '../../src/auth/auth.controller';
+import { CatsController } from '../../src/cats/cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CatsModule } from '../../src/cats/cats.module';
+import { UsersService } from '../../src/users/users.service';
+import { CatsService } from '../../src/cats/cats.service';
+
 
 describe('CatsController (E2E)', () => {
-  let app: INestApplication;
+  let app: any;
+  let httpServer: any;
+  let token: string;
+
+  console.log(process.env.DB_HOST)
+  console.log(process.env.DB_USER)
+  console.log(process.env.DB_NAME)
+
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule], // Ajusta la importación a tu AppModule
+      controllers: [CatsController],
+      providers: [UsersService, JwtModule, ConfigService, CatsService],
+      imports: [
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>("JWT_SECRET"),
+            signOptions: { expiresIn: "1d" },
+            global: true,
+          }),
+          inject: [ConfigService],
+        }),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+        AppModule,
+        CatsModule,
+      ], // Ajusta la importación a tu AppModule
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    httpServer = app.getHttpServer();
+    //token = await login();
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk0MzE0NzY5LCJleHAiOjE2OTQ0MDExNjl9.9Ri9H4XQdJ5eWgexYm7cgeOnFLy0TRnrcxfpdFgIuX8";
     await app.init();
   });
 
@@ -19,42 +62,36 @@ describe('CatsController (E2E)', () => {
     await app.close();
   });
 
-  it('/cats (POST) - Debería crear un gato', () => {
-    return request(app.getHttpServer())
-      .post('/cats')
-      .send({ /* Datos de creación del gato */ })
-      .expect(201) // Código de respuesta esperado
-      .expect({ /* Respuesta esperada */ });
+  /*const login = async () => {
+    const response = await request(httpServer).post('/api/auth/login').send({
+      username: 'admin@gmail.com',
+      password: '123456',
+    });
+
+    console.log('Justo antes de la solicitud POST');
+    console.log(response.status)
+
+    return response.body.token;
+  };
+*/
+
+  it('/cats (POST) - Debería crear un gato', async () => {
+
+    const body = {
+      "name" : "cat 3",
+      "age" : 2,
+      "breed" : "Persa"
+    }
+
+    console.log(token)
+
+    const response = await request(httpServer)
+      .post('/api/cats')
+      .set('Authorization', `bearer ${token}`)
+      .send(body);
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject(body);
   });
 
-  it('/cats (GET) - Debería obtener todos los gatos', () => {
-    return request(app.getHttpServer())
-      .get('/cats')
-      .expect(200) // Código de respuesta esperado
-      .expect([ /* Array de gatos esperado */ ]);
-  });
-
-  it('/cats/:id (GET) - Debería obtener un gato por ID', () => {
-    const catId = 1; // Cambia esto al ID de un gato existente
-    return request(app.getHttpServer())
-      .get(`/cats/${catId}`)
-      .expect(200) // Código de respuesta esperado
-      .expect({ /* Gato esperado por ID */ });
-  });
-
-  it('/cats/:id (PATCH) - Debería actualizar un gato por ID', () => {
-    const catId = 1; // Cambia esto al ID de un gato existente
-    return request(app.getHttpServer())
-      .patch(`/cats/${catId}`)
-      .send({ /* Datos de actualización del gato */ })
-      .expect(200) // Código de respuesta esperado
-      .expect({ /* Respuesta esperada después de la actualización */ });
-  });
-
-  it('/cats/:id (DELETE) - Debería eliminar un gato por ID', () => {
-    const catId = 1; // Cambia esto al ID de un gato existente
-    return request(app.getHttpServer())
-      .delete(`/cats/${catId}`)
-      .expect(200); // Código de respuesta esperado
-  });
+ 
 });
